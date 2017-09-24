0401ee87dd7e736e653994f8e880caceab8be95c
Gera Groshev <groshevg@berkeley.edu>
2017-09-23 21:59:32 -0700

    tests

--- ex2/test/TestBetting.sol
+++ ex2/test/TestBetting.sol
@@ -4,7 +4,7 @@ import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/BettingContract.sol";
 
-contract TestBettingContract {
+contract TestBetting {
 	BettingContract betting = BettingContract(DeployedAddresses.BettingContract());
 
 	function testChooseOracle() {
@@ -13,16 +13,16 @@ contract TestBettingContract {
 		Assert.equal(oracle, expected, "Oracle chosen by Owner should be registered.");
 	}
 
-	function testMakeBet() {
-		address exampleA = 0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db;
-		// betting.getCurrAddress.call({from:exampleA});
-		betting.transfer(50);
-		bool boolA = betting.makeBet(1);
-		// bool boolB = betting.makeBet({from: 0x583031d1113ad414f02576bd6afabfb302140225, value: 600});
-		address gamblerA = betting.gamblerA();
-		Assert.equal(boolA, true, "GamblerA should be set correctly.");
-		Assert.equal(gamblerA, exampleA, "GamblerA should be set to correct address.");
-	}
+	// function testMakeBet() {
+	// 	address exampleA = 0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db;
+	// 	// betting.getCurrAddress.call({from:exampleA});
+	// 	betting.transfer(50);
+	// 	bool boolA = betting.makeBet(1);
+	// 	// bool boolB = betting.makeBet({from: 0x583031d1113ad414f02576bd6afabfb302140225, value: 600});
+	// 	address gamblerA = betting.gamblerA();
+	// 	Assert.equal(boolA, true, "GamblerA should be set correctly.");
+	// 	Assert.equal(gamblerA, exampleA, "GamblerA should be set to correct address.");
+	// }
 
 	// function testCheckOutcomes() {
 	// 	uint[] outcomes;
